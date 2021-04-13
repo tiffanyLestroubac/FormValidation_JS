d4d0ccf900de75b96fa327dabf72bbf875b7f816
Tiffany LESTROUBAC <46599095+tiffanyLestroubac@users.noreply.github.com>
2021-04-13 09:39:25 +0200

    Create tcu_management.js

---
+++ tcu_management.js
@@ -0,0 +1,16 @@
+const tcuValidation = () => {
+  const tcuAcceptanceError = document.getElementById('tcu-acceptance-error');
+  if (tcuAcceptanceError) return;
+  const tcuButtonElmt = document.getElementById('checkbox');
+  if (tcuButtonElmt.checked === false) {
+    tcuButtonElmt.insertAdjacentHTML('afterend', `<p id='tcu-acceptance-error' class='error' style='color: red;'>${errorMessages.tcu.acceptance}</p>`);
+  }
+};
+
+const removeTcuErrors = () => {
+  const tcuAcceptanceError = document.getElementById('tcu-acceptance-error');
+  if (tcuAcceptanceError) tcuAcceptanceError.remove();
+};
+
+const tcuButtonElmt = document.getElementById('checkbox');
+tcuButtonElmt.addEventListener('click', removeTcuErrors);
