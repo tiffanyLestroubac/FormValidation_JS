5ee7e47ee4363bd6950b33b7170d9c0e13cf7be7
Tiffany LESTROUBAC <46599095+tiffanyLestroubac@users.noreply.github.com>
2021-04-13 09:37:54 +0200

    Create first_name_management.js

---
+++ first_name_management.js
@@ -0,0 +1,17 @@
+const firstNameValidation = () => {
+  const firstNameLengthError = document.getElementById('first-name-length-error');
+  if (firstNameLengthError) return;
+  const firstNameElmt = document.getElementById('first_name');
+  const firstNameInput = firstNameElmt.value;
+  if (firstNameInput.length < 3) {
+    firstNameElmt.insertAdjacentHTML('afterend', `<p id='first-name-length-error' class='error' style='color: red;'>${errorMessages.firstName.length}</p>`);
+  }
+};
+
+const removeFirstNameErrors = () => {
+  const firstNameLengthError = document.getElementById('first-name-length-error');
+  if (firstNameLengthError) firstNameLengthError.remove();
+};
+
+const firstNameElmt = document.getElementById('first_name');
+firstNameElmt.addEventListener('click', removeFirstNameErrors);
